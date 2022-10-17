121554b1f36e7b9115ce74232c9fc28c82bfe36f
Romero <marc.romero.ostos@gmail.com>
2022-10-17 13:37:12 +0200

    html, css, js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,36 @@
-window.body
+
+
+let displayUp = document.querySelector('.screenUp');
+let displayDown = document.querySelector('.screenDown') 
+let buttons = Array.from(document.querySelectorAll('button'));
+
+
+
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC':
+                displayUp.innerText = '0';
+                displayDown.innerText = '0';
+                break;
+
+            case 'C':
+                if(displayUp.innerText){
+                    displayUp.innerText = displayUp.innerText.slice(0, -1)
+                }
+                break;
+
+            case '=':
+                try{
+                    displayDown.innerText = eval(displayUp.innerText);
+                }catch{
+                    displayDown.innerText = 'Error!';
+                }
+                break;
+
+            default:
+                displayUp.innerText += e.target.innerText;
+        }
+    });
+});
